e13062c874040c8b74962ffafb0c560db6aa17f6
ConradSkelly <conradskelly@gmail.com>
2025-08-11 13:15:45 +0200

    sim gryo commit

---
+++ Swerve-Training/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.units.measure.*;
+
+public class Constants {
+
+    public static class SwerveConstants{
+        
+
+    }
+    
+}
---
+++ Swerve-Training/src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,56 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Second;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.swerve.jni.SwerveJNI.ModuleState;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.Kinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.units.*;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import frc.robot.Constants.SwerveConstants;;
+
+@Logged
+public class Swerve extends SubsystemBase {
+
+  private boolean isBlue;
+  private Pose2d estimatedPose;
+  private SwerveModuleState[] moduleStates;
+  private ChassisSpeeds chassisSpeeds;  
+  private Rotation2d simHeading;
+
+    public Swerve(){
+
+      estimatedPose = new Pose2d();
+      simHeading = new Rotation2d();
+
+      moduleStates = new SwerveModuleState[4];
+    }
+
+    public void setIsBlue(Boolean allianceColour){
+      isBlue = allianceColour;
+      }
+
+    private void fieldRelitiveDrive(LinearVelocity x, LinearVelocity y, AngularVelocity omega){
+      simHeading = new Rotation2d(omega.times(Second.of(.02)));
+      estimatedPose = new Pose2d(((x.baseUnitMagnitude() / 20) * (isBlue ? -1 : 1) + estimatedPose.getX()), (y.baseUnitMagnitude() / 20) * (isBlue ? -1 : 1) + estimatedPose.getY(), simHeading.plus(new Rotation2d (estimatedPose.getRotation().getRadians())));
+
+    }
+
+    public Command driveCommand(DoubleSupplier x, DoubleSupplier y, DoubleSupplier omega){
+      return Commands.run(() -> fieldRelitiveDrive(MetersPerSecond.of(x.getAsDouble()), MetersPerSecond.of(y.getAsDouble()), RotationsPerSecond.of(omega.getAsDouble())), this);
+    }
+}
