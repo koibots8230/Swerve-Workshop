727daec11e30a4cc9ee531b74ba603887f70abac
ConradSkelly <conradskelly@gmail.com>
2025-08-11 18:31:41 +0200

    restored start (except readme)

---
+++ Swerve-Training/src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;;
+
+@Logged
+public class Swerve extends SubsystemBase {
+
+    public Swerve(){
+
+    }
+
+}
