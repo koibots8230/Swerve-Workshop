33dd9777682f3e65a1ce94a96749e1b94df1cc9c
NotABotDaniel <dcgottshall@icloud.com>
2025-10-05 21:40:54 -0400

    figured out swerve subsystem. Can't figure out why RobotContainer doesen't work.

--- Swerve-Training/src/main/java/frc/robot/RobotContainer.java
+++ Swerve-Training/src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,14 @@
 package frc.robot;
 
+import java.io.Console;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.*;
 
 @Logged
@@ -12,18 +16,38 @@ public class RobotContainer {
   
   private final Swerve swerve;
 
+  private final CommandXboxController xboxKid;
+
   public RobotContainer() {
 
     swerve = new Swerve();
 
+    xboxKid = new CommandXboxController(0);
 
     configureBindings();
     defualtCommands();
   }
 
-  private void configureBindings() {}
+  
+
+  private void configureBindings() {
+    swerve.setDefaultCommand(
+      swerve.happyMeal(
+        Double.valueOf(xboxKid::getLeftY), 
+        Double.valueOf(xboxKid::getLeftX), 
+        Double.valueOf(xboxKid::getRight)
+      )
+    );
+  
+    Trigger buttonA = xboxKid.a();
+    buttonA.onTrue(System.out.println("A button pressed"))
+    buttonA.onFalse(System.out.println("A button released"));
+  
+  }
+
 
   private void defualtCommands() {}
+
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- Swerve-Training/src/main/java/frc/robot/subsystems/Swerve.java
+++ Swerve-Training/src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,10 +1,22 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 @Logged
 public class Swerve extends SubsystemBase {
 
   public Swerve() {}
+
+  private void beHappy(double blah1, double blah2, double blah3) {
+    System.out.println("I'm happy!");
+    System.out.println(blah1 + " " + blah2 + " " + blah3);
+  }
+
+  public Command happyMeal(double blah1, double blah2, double blah3) {
+    return Commands.runOnce(() -> this.beHappy(blah1, blah2, blah3), this);
+  
+  }
 }
